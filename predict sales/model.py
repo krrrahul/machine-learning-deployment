01978b2acac43c9881e5395e8ae969ca2381b419
Rahul Kumar <c-rahul.kumar@icertis.com>
2021-06-08 21:44:44 +0530

    'modified'

--- predict sales/model.py
+++ predict sales/model.py
@@ -2,30 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
-
-dataset = pd.read_csv('sales.csv')
-
-dataset['rate'].fillna(0, inplace=True)
-
-dataset['sales_in_first_month'].fillna(dataset['sales_in_first_month'].mean(), inplace=True)
-
-X = dataset.iloc[:, :3]
-
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['rate'] = X['rate'].apply(lambda x : convert_to_int(x))
-
-y = dataset.iloc[:, -1]
-
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, median_absolute_error
+
+df = pd.read_csv('../data/end-part2_df.csv').set_index('date')
+
+predictors = ['meantempm_1',  'meantempm_2',  'meantempm_3',
+              'mintempm_1',   'mintempm_2',   'mintempm_3',
+              'meandewptm_1', 'meandewptm_2', 'meandewptm_3',
+              'maxdewptm_1',  'maxdewptm_2',  'maxdewptm_3',
+              'mindewptm_1',  'mindewptm_2',  'mindewptm_3',
+              'maxtempm_1',   'maxtempm_2',   'maxtempm_3']
+df2 = df[['meantempm'] + predictors]
+
+X = df2[predictors]
+y = df2['meantempm']
+X = X[['mintempm_1', 'mintempm_2', 'mintempm_3', 'maxdewptm_1', 'maxdewptm_3', 'mindewptm_1', 'maxtempm_1']]
+print(X.columns)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+# instantiate the regressor class
 regressor = LinearRegression()
 
-regressor.fit(X, y)
+# fit the build the model by fitting the regressor to the training data
+regressor.fit(X_train, y_train)
 
+# make a prediction set using the test set
+prediction = regressor.predict(X_test)
+
+# Evaluate the prediction accuracy of the model
+
+print("The Explained Variance: %.2f" % regressor.score(X_test, y_test))
+print("The Mean Absolute Error: %.2f degress celcius" % mean_absolute_error(y_test, prediction))
+print("The Median Absolute Error: %.2f degrees celcius" % median_absolute_error(y_test, prediction))
+#
 pickle.dump(regressor, open('model.pkl','wb'))
-
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[4, 300, 500]]))
